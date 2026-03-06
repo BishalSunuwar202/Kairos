aaa7d4fc38a3a5d2de5dfaa0d90d08cd16fc5254
BishalSunuwar202 <bishalsunuwar24@gmail.com>
2026-03-06 22:23:56 +0545

    fix: reload church logo from localStorage when presentation starts

--- src/components/slide-viewer.tsx
+++ src/components/slide-viewer.tsx
@@ -22,7 +22,7 @@ export function SlideViewer() {
 
   useEffect(() => {
     setLogoUrl(localStorage.getItem('kairos_church_logo'))
-  }, [])
+  }, [isPresenting])
 
   useEffect(() => {
     function handleKeyDown(e: KeyboardEvent) {
